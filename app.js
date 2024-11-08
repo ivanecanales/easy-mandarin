1c48cbc8610dd0a1edf88a6d7181ff90ff044fab
Ivan Canales <Ivan.Canales@sqm.com>
2024-11-08 11:29:57 -0300

    refactored js

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
+const element = id => document.getElementById(id);
+const start = 50;
+const end = 60;
+const delay = 3000; //slow for study, fast for playing
 fetch('data/hanzi.json')
   .then(response => response.json())
-  .then(data => startGame(data.slice(0, 50)))
+  .then(data => startGame(data.slice(start, end)))
 let items = [];
 let currentItemIndex = 0;
 let usedIndices = [];
 function startGame(data) {
   items = data;
-  displayRandomHint();
+  shownextcard();
 }
-const audio = document.getElementById('audio');
-function displayRandomHint() {
+const [hanzi, english, audio, pinyin, user, result] = [
+  'hanzi', 'english', 'audio', 'pinyin', 'user', 'result'
+].map(id => element(id))
+function shownextcard() {
   if (usedIndices.length === items.length) {
-    document.getElementById('result').textContent = "You've completed all the items!";
-    document.getElementById('hanzi').textContent = "";
-    document.getElementById('english').textContent = "";
-    document.getElementById('user').value = "";
+    result.textContent = "you've completed all hanzi!";
+    [hanzi, english, user].forEach(
+      element => {element.style.display = 'none'}
+    );
+    /*hanzi.textContent = "";
+    english.textContent = "";
+    user.value = "";*/
     return;
   }
   do {
@@ -23,7 +32,7 @@ function displayRandomHint() {
   document.getElementById('hanzi').textContent = items[currentItemIndex].hanzi;
   document.getElementById('user').value = '';
   document.getElementById('english').textContent = items[currentItemIndex].english.join(', ');
-  document.getElementById('audio').src = `data/audio/${String(currentItemIndex + 1).padStart(4, '0')}.mp3`;
+  audio.src = `data/audio/${String(start + currentItemIndex + 1).padStart(4, '0')}.mp3`;
   document.getElementById('result').textContent = '';
 }
 function submitAnswer() {
@@ -31,13 +40,12 @@ function submitAnswer() {
   if (userAnswer.toLowerCase() === items[currentItemIndex].pinyin.toLowerCase()) {
     document.getElementById('result').textContent = "correct!";
     usedIndices.push(currentItemIndex);
-    setTimeout(displayRandomHint, 3000);
+    setTimeout(shownextcard, delay);
   } else {
     document.getElementById('result').textContent = `Incorrect! Answer is ${items[currentItemIndex].pinyin}`;
-    setTimeout(displayRandomHint, 3000);
+    setTimeout(shownextcard, delay);
   }
 }
-const user = document.getElementById('user');
 const source = [
   '-a', '-e', '-i', '-o', '-u',
   '.a', '.e', '.i', '.o', '.u',
